27aa2383b33f226cd7aaa1cfb6e6541439445470
Sandhya <sandhyaranni09@gmail.com>
2026-03-30 23:31:10 -0400

    Test NG Annotations (Used Before Method and After Method)

--- PractiseSelenium/src/test/java/testingWebElements/BaseForJavaScript.java
+++ PractiseSelenium/src/test/java/testingWebElements/BaseForJavaScript.java
@@ -8,32 +8,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseForJavaScript {
 
 	WebDriver driver;
 	WebDriverWait wait;
-	
-	
+
+	@BeforeMethod
 	public void preReq() {
-		
-	 driver = new ChromeDriver();
-	    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.navigate().to("https://the-internet.herokuapp.com/");
 
 		WebElement jsAlertsEle = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='JavaScript Alerts']")));
 
 		jsAlertsEle.click();
-		
-		
+
 	}
 
-	
+	@AfterMethod
 	public void tearDown() {
-		
+
 		driver.quit();
 		System.out.println("Browser is closed");
-		
+
 	}
 }
--- PractiseSelenium/src/test/java/testingWebElements/JavaScriptAlertsDRY.java
+++ PractiseSelenium/src/test/java/testingWebElements/JavaScriptAlertsDRY.java
@@ -1,33 +1,24 @@
 package testingWebElements;
 
-import java.time.Duration;
-
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class JavaScriptAlertsDRY  {
-	
-	WebDriver driver;
-	WebDriverWait wait;
-	
-	
+public class JavaScriptAlertsDRY extends BaseForJavaScript {
+
+//	WebDriver driver;
+//	WebDriverWait wait;
+
 	@Test
 
 	public void jsAlerts() {
-		 
-		BaseForJavaScript base = new BaseForJavaScript();
-		
-		base.preReq();
 
-		wait = base.wait;
-		driver = base.driver;
+		// preReq();
+
 		WebElement jsAlertEle = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Alert']")));
 
@@ -42,18 +33,13 @@ public class JavaScriptAlertsDRY  {
 		Assert.assertEquals(message, "You successfully clicked an alert");
 		System.out.println(message);
 
-		base.tearDown();
+		// tearDown();
 	}
 
 	@Test
 	public void clickForJSConfirm() {
-		
-		BaseForJavaScript base = new BaseForJavaScript();
-		base.preReq();
-		
-		wait = base.wait;
-		driver = base.driver;
-	
+
+		// preReq();
 
 		WebElement clickJSConfirm = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Confirm']")));
@@ -69,20 +55,14 @@ public class JavaScriptAlertsDRY  {
 		Assert.assertEquals(message, "You clicked: Ok");
 		System.out.println(message);
 
-		base.tearDown();
+		// tearDown();
 
 	}
-	
+
 	@Test
 
 	public void clickForJSPrompt() {
-
-		BaseForJavaScript base = new BaseForJavaScript();
-		
-		base.preReq();
-		
-		wait = base.wait;
-		driver = base.driver;
+		// preReq();
 
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Click for JS Prompt']"))).click();
 
@@ -94,9 +74,9 @@ public class JavaScriptAlertsDRY  {
 		String msg = driver.findElement(By.xpath("//p[@id='result']")).getText();
 
 		Assert.assertEquals(msg, "You entered: Sandhya");
-		
+
 		System.out.println(msg);
 
-		base.tearDown();
+		// tearDown();
 	}
 }
