d79a73082c380a6599ae36ab8bdf79e4ec56cf7f
Sandhya <sandhyaranni09@gmail.com>
2026-04-13 00:11:12 -0400

    added  report code

--- PractiseSelenium/src/test/java/testingWebElements/BaseForJavaScript.java
+++ PractiseSelenium/src/test/java/testingWebElements/BaseForJavaScript.java
@@ -1,5 +1,7 @@
 package testingWebElements;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -14,56 +16,77 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import managers.ExtentManager;
+import managers.ExtentTestManager;
 
 public class BaseForJavaScript {
 
 	WebDriver driver;
 	WebDriverWait wait;
 
+	@BeforeSuite
+	public void initialise() throws IOException {
+		ExtentManager.initReport();
+	}
+
 	@BeforeMethod
-	public void preReq() {
-		
+	public void preReq(Method method) {
+
+		ExtentTestManager.createTest(method.getName());
 		String browser = "Chrome";
 //		
 //		if (browser.equals("Chrome")) {
 //		
 //		driver = new ChromeDriver();
 //		}
-		
+
 		switch (browser.toLowerCase()) {
 		case "Chrome":
 			driver = new ChromeDriver();
-			
+			ExtentTestManager.log.info("Chrome Browser was launched succcessfully !!");
+
 		case "Firefox":
-			
+
 			driver = new FirefoxDriver();
-			
+			ExtentTestManager.log.info("Firefox Browser was launched succcessfully !!");
+
 		case "Edge":
 			driver = new EdgeDriver();
-			
+			ExtentTestManager.log.info("Edge Browser was launched succcessfully !!");
+
 		case "Safari":
 			driver = new SafariDriver();
-			
+			ExtentTestManager.log.info("Safari Browser was launched succcessfully !!");
+
 		case "InternetExplorerDriver":
 			driver = new InternetExplorerDriver();
-			
-			default:
-				System.out.println("Browser not Supported !");
-		
+			ExtentTestManager.log.info("IE Browser was launched succcessfully !!");
+
+		default:
+			System.out.println("Browser not Supported !");
+			ExtentTestManager.log.info("No Matching Browser was found  !!");
+			break;
+
 		}
-		
+
 		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		driver.navigate().to("https://the-internet.herokuapp.com/");
-
-		
+		ExtentTestManager.log.info("Navigated to the url");
 
 	}
 
 	@AfterMethod
-	public void tearDown() {
+	public void endTest() {
 
 		driver.quit();
-		System.out.println("Browser is closed");
+		//System.out.println("Browser is closed");
+		ExtentTestManager.log.info("Browser window was closed succcessfully !!");
 
+	}
+
+	public void tearDown() {
+		ExtentManager.flushReport();
 	}
 }
