ed8dd597cf81e8b8c979df9eca8dc1788632f082
Sandhya <sandhyaranni09@gmail.com>
2026-04-26 00:50:28 -0400

    Sort by enrollments

--- PractiseSelenium/src/test/java/testingWebElements/SortForTestTable.java
+++ PractiseSelenium/src/test/java/testingWebElements/SortForTestTable.java
@@ -1,22 +1,43 @@
 package testingWebElements;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SortForTestTable extends BaseForTestTable{
-	
+public class SortForTestTable extends BaseForTestTable {
+
 	@Test
-	
-	public void sortByEnrol () {
-		
-		
+
+	public void sortByEnrol() {
+
 		driver.findElement(By.xpath("//select[@id='sortBy']")).click();
 
 		driver.findElement(By.xpath("//select[@id='sortBy']/option[@value='col_enroll']")).click();
-		
-		
-	} 
-	
-	
+
+		List<WebElement> elements = driver.findElements(By.xpath("//table/tbody/tr/td[5]"));
+
+		// Get actual list values from table
+
+		List<Integer> actualList = new ArrayList<>();
+		for (WebElement el : elements) {
+			String values = el.getText();
+			int number = Integer.parseInt(values.replaceAll("[^0-9]", ""));
+			actualList.add(number);
+			System.out.println(actualList);
+
+			List<Integer> sortedList = new ArrayList<>(actualList);
+			Collections.sort(sortedList);
+			System.out.println(sortedList);
+
+			Assert.assertEquals(actualList, sortedList, "List is not sorted!");
+
+		}
+
+	}
 
 }
