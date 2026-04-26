b00cec466b943f4b163de132685aadd8db6a630b
Sandhya <sandhyaranni09@gmail.com>
2026-04-26 18:37:11 -0400

    Full code for Sort by course name test case

--- PractiseSelenium/src/test/java/testingWebElements/SortForTestTable.java
+++ PractiseSelenium/src/test/java/testingWebElements/SortForTestTable.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -41,29 +42,50 @@ public class SortForTestTable extends BaseForTestTable {
 	}
 
 	@Test
-	public void sortByCourse() {
-
-		driver.findElement(By.xpath("//select[@id='sortBy']")).click();
-
-		driver.findElement(By.xpath("//select[@id='sortBy']/option[@value='col_course']")).click();
-
-		List<WebElement> elements = driver.findElements(By.xpath("//table/tbody/tr/td[2]"));
-
-		List<String> actualList = new ArrayList<>();
-
-		for (WebElement i : elements) {
-			String values = i.getText();
-			actualList.add(values);
+		public void sortByCourse() {
+			
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//select[@id='sortBy']"))).click();
+	
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//select[@id='sortBy']/option[@value='col_course']"))).click();
+	
+			List<WebElement> elements = driver.findElements(By.xpath("//table/tbody/tr/td[2]"));
+	
+			List<String> actualList = new ArrayList<>();
+	
+			for (WebElement i : elements) {
+				String values = i.getText();
+				actualList.add(values);
+			}
+	
+			List<String> sortedList = new ArrayList<>(actualList);
+			Collections.sort(sortedList);
+	
+			System.out.println("Actual List " + actualList);
+			System.out.println("Sorted List " + sortedList);
+	
+			Assert.assertEquals(actualList, sortedList, "List is not sorted!");
+	
+			
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()=' Java']"))).click();
+			
+			List<WebElement> filteredElements = driver.findElements(By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]/td[2]"));
+	
+			List<String> actualFilteredList = new ArrayList<>();
+	
+			for (WebElement fil : filteredElements) {
+				String filteredvalues = fil.getText();
+				actualFilteredList.add(filteredvalues);
+			}
+	
+			List<String> sortedFilteredList = new ArrayList<>(actualFilteredList);
+			Collections.sort(sortedFilteredList);
+	
+			System.out.println("Actual List " + actualFilteredList);
+			System.out.println("Sorted List " + sortedFilteredList);
+	
+			Assert.assertEquals(actualFilteredList, sortedFilteredList, "List is not sorted!");
+			
+			
 		}
-
-		List<String> sortedList = new ArrayList<>(actualList);
-		Collections.sort(sortedList);
-
-		System.out.println("Actual List " + actualList);
-		System.out.println("Sorted List " + sortedList);
-
-		Assert.assertEquals(actualList, sortedList, "List is not sorted!");
-
+	
 	}
-
-}
